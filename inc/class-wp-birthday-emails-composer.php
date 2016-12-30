6db3c59796a35a57c3b8a0813cf99bd2cd0ca209
Alberto Vargas <avargas@frontuari.com>
2016-12-30 01:54:41 -0400

    Add test email multiples

--- inc/class-wp-birthday-emails-composer.php
+++ inc/class-wp-birthday-emails-composer.php
@@ -10,6 +10,9 @@ class WPBirthdayemails_Composer {
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_menu' ) );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
+		add_action( 'admin_footer', array($this,'reparaciones_load_scripts' ));
+		add_action( 'wp_ajax_email_send_p',  array(__CLASS__,'email_send_p_callback' ));
+		
 	}
 
 	/**
@@ -26,6 +29,62 @@ class WPBirthdayemails_Composer {
 		);
 	}
 
+
+	//funcion ajax para probar
+	public static function email_send_p_callback(){
+		check_ajax_referer('email_send_ajax' );
+		$option  = get_option( 'wp-birthday-emails');
+		$content_temp = '';
+		//data mail
+		$subject = isset( $option['title'] ) ? $option['title'] : '';
+		$content = isset( $option['content'] ) ? $option['content'] : '';
+		$emails  = WPBirthdayemails_List_User::get_list_user_birthday();
+		for($i=0; $i<count($emails);$i++){
+			$content_temp =WPBirthdayemails_Cron::replace_content($content,$emails[$i]);
+			wp_mail( $emails[$i], $subject, $content_temp, array( 'Content-Type: text/html; charset=UTF-8' ) );
+			sleep(2);	
+		}
+		echo 'enviado';
+	}
+
+	//funcion para llamar a un javascript
+	public  function reparaciones_load_scripts(){
+		$nonce = wp_create_nonce('email_send_ajax');
+	?>
+
+	<script type="text/javascript">
+		jQuery(document).ready(function($){
+
+				//creando la funcion ajax
+				function ajax_post(){
+					var data = {
+						titulo : 'efecto ajax'
+					}
+					$.post( 
+							"<?php echo admin_url( 'admin-ajax.php' ); ?>", 
+							{
+							action : "email_send_p",
+							_ajax_nonce : "<?php echo $nonce; ?>",
+							data:data
+							},
+							function( result ) {
+								if(result.indexOf('enviado')>-1){
+									$("#send_email_div").text("MENSAJE ENVIADO").delay(1000).fadeOut(1000);
+								}
+						});
+				}
+				//abrimos el boton download
+				$("#send_p").click(function(){
+					$("#send_email_div").show('fast');
+					$("#send_email_div").text("ENVIANDO MENSAJE...");
+					ajax_post();
+				});
+		});
+	</script>
+
+	<?php
+	}
+
 	/**
 	 * Display form in admin page with field.
 	 */
@@ -37,7 +96,11 @@ class WPBirthdayemails_Composer {
 				settings_fields( 'wpbirthdayemails_setting' );
 				do_settings_sections( 'wp-birthday-emails-composer' );
 				submit_button();
+
 				?>
+				 <input type="button"  name="send_p" id="send_p" class="button button-primary" value="Probar Correo">
+				<span style="display:none; margin-left: 10px; font-size: 16px; color:black; font-weight: bold;" id="send_email_div">ENVIANDO MENSAJE..</span>
+
 			</form>
 		</div>
 		<?php
